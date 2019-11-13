cd5303d8747e83c821a6f4b82a356c754b6c0c1b
Yauheni Krauchanka1 <Yauheni_Krauchanka1@epam.com>
2019-11-13 17:15:53 +0300

    HW3 add onInit to fix input

--- src/app/courses-page-module/courses-list-item/course-status.directive.ts
+++ src/app/courses-page-module/courses-list-item/course-status.directive.ts
@@ -1,19 +1,20 @@
-import {Directive, Renderer2, ElementRef, Input} from '@angular/core';
+import {Directive, Renderer2, ElementRef, Input, OnInit} from '@angular/core';
 
 @Directive({
   selector: '[appCourseStatus]'
 })
-export class CourseStatusDirective {
+export class CourseStatusDirective implements OnInit {
+  constructor(private element: ElementRef, private renderer: Renderer2) {}
+
   @Input('appCourseStatus') creationDate: string;
 
-  constructor(private element: ElementRef, private renderer: Renderer2) {
+  ngOnInit() {
     this.renderer.setStyle(this.element.nativeElement, 'border-color', this.setBorderColorByDate());
   }
 
   private setBorderColorByDate(): string {
     const currentDate = new Date();
-    // const creationDate = new Date(this.creationDate);
-    const creationDate = new Date('11/09/2019');
+    const creationDate = new Date(this.creationDate);
     const diffDays = Math.ceil(Math.abs(currentDate.getDate() - creationDate.getDate()) / (1000 * 60 * 60 * 24));
     let color = 'transparent';
 
