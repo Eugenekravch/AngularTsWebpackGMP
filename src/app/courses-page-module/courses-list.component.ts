322bac9561c519c9d7f289fb55e0536c0841824d
Yauheni Krauchanka1 <Yauheni_Krauchanka1@epam.com>
2019-11-19 16:12:48 +0300

    HW5 add courses service with methods

--- src/app/courses-page-module/courses-list.component.ts
+++ src/app/courses-page-module/courses-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CoursesService } from './courses.service';
 import {CoursesListItem} from './courses-list.interface';
 
 @Component({
@@ -7,45 +8,13 @@ import {CoursesListItem} from './courses-list.interface';
   styleUrls: ['./courses-list.component.scss']
 })
 export class CoursesListComponent implements OnInit {
-  courses: CoursesListItem[] = [
-    {
-      id: 1,
-      title: 'course 1',
-      creationDate: new Date('10/11/2019'),
-      description: '',
-      duration: 110,
-      topRated: false
-    },
-    {
-      id: 2,
-      title: 'course 2',
-      creationDate: new Date('11/11/2019'),
-      description: 'On this training, we will take a look at custom ' +
-        'components in Angular 2, what they are, how to build comp' +
-        'onents in Angular 2 application and pass data between ' +
-        'them. Also, we will check components lifecy' +
-        'cle and find out correct component’s li' +
-        'fecycle event for several common tasks you’ll face during development. ',
-      duration: 10,
-      topRated: true
-    },
-    {
-      id: 3,
-      title: 'course 3',
-      creationDate: new Date('12/12/2019'),
-      description: 'On this training, we will take a look at custom ' +
-        'components in Angular 2, what they are, how to build comp' +
-        'onents in Angular 2 application and pass data between ' +
-        'them. Also, we will check components lifecy' +
-        'cle and find out correct component’s li' +
-        'fecycle event for several common tasks you’ll face during development. ',
-      duration: 61,
-      topRated: false
-    }];
 
-  constructor() { }
+  courses: CoursesListItem[];
+
+  constructor(private coursesService: CoursesService) { }
 
   ngOnInit() {
+    this.getCourses();
   }
 
   loadMore() {
@@ -53,6 +22,10 @@ export class CoursesListComponent implements OnInit {
   }
 
   deleteCourse(id: number) {
-    console.log(id);
+    this.coursesService.removeItem(id);
+  }
+
+  getCourses(): void {
+    this.courses = this.coursesService.getList();
   }
 }
